ba06dfeb76354f791765dca7ea18ca2c024719e4
HornyDragoness <NightwayGlory@yandex.by>
2020-02-02 12:44:14 +0300

    rePush

--- scrp.py
+++ scrp.py
@@ -181,3 +181,23 @@ class Feedback(db.Model):
 
 if __name__ == "__main__":
 	manager.run()
+def flag(N):
+	class ArgumentError(Exception):
+		pass
+
+	if not isinstance(N, int) or N % 2 != 0 or N <= 0:
+		raise ArgumentError("N should be an integer even number")
+
+	# Creating the lines before the circle
+	output_string = ('#' * (N * 3 + 2) + '\n')
+	for i in range(int(N / 2)):
+		output_string += ('#' + ' ' * (N * 3) + '#\n')
+
+	# Creating first half of the circle
+	for i in range(0, N, 2):
+		output_string += ('#' + ('*' + 'o' * i + '*').center(3 * N, ' ') + '#\n')
+
+	# Second half of the circle and the lines are mirrored
+	output_string += output_string[-2::-1]
+
+	return output_string
